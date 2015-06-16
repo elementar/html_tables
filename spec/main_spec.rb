9fa8425220a8dc03c36684195f76e91ab3f6d666
Fábio D. Batista <fabio@elementarsistemas.com.br>
2015-06-15 23:13:11 -0300

    Added some pending tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -10,37 +10,61 @@ RSpec.describe HtmlTables::DataTable do
   let(:col1) { [{ id: 1, name: 'Record One' }, { id: 2, name: 'Record Two' }].map &OpenStruct.method(:new) }
   let(:builder) { FakeBuilder.new }
 
-  it 'should render nicely when empty' do
-    html = builder.data_table_for col0 do |t|
-      t.column :id
-      t.column :name
-      t.nodata 'No records found'
+  describe 'basic usage' do
+    it 'should render nicely when empty' do
+      html = builder.data_table_for col0 do |t|
+        t.column :id
+        t.column :name
+        t.nodata 'No records found'
+      end
+
+      expect(html).to have_tag :tbody do
+        with_tag :tr, count: 1 do
+          with_tag :td, text: 'No records found', with: { colspan: 2 }
+        end
+      end
     end
 
-    expect(html).to have_tag :tbody do
-      with_tag :tr, count: 1 do
-        with_tag :td, text: 'No records found', with: { colspan: 2 }
+    it 'should render nicely with records and explicit columns' do
+      html = builder.data_table_for col1 do |t|
+        t.column :id
+        t.column :name
+        t.nodata 'No records found'
+      end
+
+      expect(html).not_to have_tag :td, text: 'No records found'
+      expect(html).to have_tag :tbody do
+        with_tag :tr do
+          with_tag :td, text: '1'
+          with_tag :td, text: 'Record One'
+        end
+        with_tag :tr do
+          with_tag :td, text: '2'
+          with_tag :td, text: 'Record Two'
+        end
       end
     end
   end
 
-  it 'should render nicely with records and explicit columns' do
-    html = builder.data_table_for col1 do |t|
-      t.column :id
-      t.column :name
-      t.nodata 'No records found'
-    end
+  describe 'totals' do
+    it 'should calculate totals correctly'
+  end
 
-    expect(html).not_to have_tag :td, text: 'No records found'
-    expect(html).to have_tag :tbody do
-      with_tag :tr do
-        with_tag :td, text: '1'
-        with_tag :td, text: 'Record One'
-      end
-      with_tag :tr do
-        with_tag :td, text: '2'
-        with_tag :td, text: 'Record Two'
-      end
-    end
+  describe 'grouping' do
+    it 'should group records correctly'
+  end
+
+  describe 'column definition' do
+    it 'should auto-detect columns from ActiveModel compatible models'
+    it 'should allow custom columns'
+  end
+
+  describe 'alignment' do
+    it 'should apply the correct classes when aligning columns'
+  end
+
+  describe 'headers' do
+    it 'should allow overriding the header text'
+    it 'should allow overriding the header style'
   end
 end
